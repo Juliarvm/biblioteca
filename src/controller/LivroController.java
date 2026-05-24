186a78095ed605235b6446bc8ecfd202975de7be
Júlia Rodrigues Vasconcellos Melo <100995393+Juliarvm@users.noreply.github.com>
2026-05-24 11:20:15 -0300

    Update LivroController.java

--- src/controller/LivroController.java
+++ src/controller/LivroController.java
@@ -1,35 +1,39 @@
 package controller;
 
 import dao.LivroDAO;
+import dao.ReservaDAO;
 import model.Livro;
 
 import java.io.IOException;
 import java.util.List;
 
 public class LivroController {
-	private final LivroDAO livroDAO;
+    private final LivroDAO livroDAO;
+    private final ReservaDAO reservaDAO;
 
-	public LivroController(LivroDAO livroDAO) {
-		this.livroDAO = livroDAO;
-	}
+    public LivroController(LivroDAO livroDAO, ReservaDAO reservaDAO) {
+        this.livroDAO = livroDAO;
+        this.reservaDAO = reservaDAO;
+    }
 
-	public int criar(Livro livro) throws IOException {
-		return livroDAO.create(livro);
-	}
+    public int criar(Livro livro) throws IOException {
+        return livroDAO.create(livro);
+    }
 
-	public Livro buscarPorId(int id) throws IOException {
-		return livroDAO.read(id);
-	}
+    public Livro buscarPorId(int id) throws IOException {
+        return livroDAO.read(id);
+    }
 
-	public List<Livro> listarTodos() throws IOException {
-		return livroDAO.readAll();
-	}
+    public List<Livro> listarTodos() throws IOException {
+        return livroDAO.readAll();
+    }
 
-	public boolean atualizar(Livro livro) throws IOException {
-		return livroDAO.update(livro);
-	}
+    public boolean atualizar(Livro livro) throws IOException {
+        return livroDAO.update(livro);
+    }
 
-	public boolean excluir(int id) throws IOException {
-		return livroDAO.delete(id);
-	}
+    public boolean excluir(int id) throws IOException {
+        reservaDAO.cancelarPorLivro(id);
+        return livroDAO.delete(id);
+    }
 }
